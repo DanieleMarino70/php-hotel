a78e004acff0774cad2ee86eeb886ba67ad17cbc
DanieleMarino70 <danielaotitan@gmail.com>
2023-03-11 18:50:22 +0100

    add: table

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+// ** Iniziate in modo graduale.
+
+// // Prima stampate in pagina i dati, senza preoccuparvi dello stile. 
+// // Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+
+
+// Bonus
+// 1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+// 2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+// NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+// Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
 
     $hotels = [
 
@@ -58,13 +69,42 @@
 <body>
     <div class="container">
         <ul>
-            <?php foreach($hotels as $key => $hotel){        ?>
-
-                <?php foreach($hotel as $infohotel){        ?>
-                    <li><?php echo $infohotel?></li>
-                <?php } ?>
-            <?php } ?>
+            
         </ul>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to Center</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($hotels as $key => $hotel){        ?>
+                    <tr>
+                    <th scope="row"><?php echo $key + 1 ?></th>
+                    <?php foreach($hotel as $name_key => $infohotel){        ?>
+                        <?php if($name_key === "parking") :    ?>
+                            <?php if($infohotel === true) :    ?>
+                                <td><?php echo "true"?></td>
+                            <?php else : ?>
+                                <td><?php echo "false"?></td>
+                            <?php endif ?>
+                        <?php else : ?>
+                        <td><?php echo $infohotel?></td>
+                        <?php endif ?>
+                    <?php } ?>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+            
+
+    
     </div>
 </body>
 </html>
